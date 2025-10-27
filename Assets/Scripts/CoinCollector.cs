ebad23258f6fa53f3cd034d44096d3e60bfd0750
bandiKOtik <muxinde2003@gmail.com>
2025-10-27 20:22:40 +0300

    Fix 2

--- Assets/Scripts/CoinCollector.cs
+++ Assets/Scripts/CoinCollector.cs
@@ -6,17 +6,12 @@ public class CoinCollector : MonoBehaviour
     public int CollectedCoinsCount { get; private set; }
     public int CollectedCoinsValue {  get; private set; }
 
-    public void AddCoin(int amount)
-    {
-        CollectedCoinsCount++;
-        CollectedCoinsValue += amount;
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent<Coin>(out Coin coin))
         {
-            AddCoin(coin.CoinValue);
+            CollectedCoinsCount++;
+            CollectedCoinsValue += coin.CoinValue;
             coin.gameObject.SetActive(false);
         }
     }
